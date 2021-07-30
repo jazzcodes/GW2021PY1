e2a2bbc86f114c103b99ffa2763194a9dbe5da55
admin <er.ishant@gmail.com>
2021-07-30 13:05:26 +0530

    Session11 Assignment with Partial Implementation

--- Session11.py
+++ Session11.py
@@ -47,9 +47,58 @@
 
 
 """
+
+months = {
+    0: "January",
+    1: "February",
+    2: "March",
+    3: "April",
+    4: "May",
+    5: "June",
+    6: "July",
+    7: "August",
+    8: "September",
+    9: "October",
+    10: "November",
+    11: "December"
+}
+
 # Empty List for Transactions
 transactions = []
 fix_amount = 0
+
+def transact_amount(amount):
+    if len(transactions) == 0:
+        transaction_record = {
+            "month": 0,
+            "amount": amount,
+            "charges": amount * 0.10,
+            "total": amount + amount * 0.10,
+            "dues": amount + amount * 0.10,
+        }
+        transactions.append(transaction_record)
+    else:
+        transaction_record = {
+            "month": transactions[len(transactions)-1]["month"]+1,
+            "amount": amount,
+            "charges": amount * 0.10,
+            "total": amount + amount * 0.10,
+            "dues": (amount + amount * 0.10) + transactions[len(transactions)-1]["dues"],
+        }
+        transactions.append(transaction_record)
+
+    print("Amount Transacted \u20b9", amount)
+
+
+def show_transaction_history():
+    for transaction in transactions:
+        print("-"*30)
+        print("Month: {}".format(months[transaction["month"]]))
+        print("Transacted Amount: \u20b9{amount}"
+              "\nBilling Amount: \u20b9{total}\nTotal Dues: \u20b9{dues}".format_map(transaction))
+        print("-" * 30)
+        print()
+
 
 def set_fix_amount(amount):
     fix_amount = amount
@@ -64,14 +113,26 @@ def show_welcome_message():
     print("[1] Enter Fixed Minimum Amount Payable Every Month")
     print("[2] Transact Any Amount")
     print("[3] Make a Payment. (Fixed Amount will be deducted automatically)")
+    print("[4] Transaction History")
+    print("[5] Quit")
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
 
 def make_choice():
-    choice = int(input("Enter Your Choice: "))
-    if choice == 1:
-        amount = int(input("Enter a Fix Amount to be paid every month: "))
-        set_fix_amount(amount)
+    while True:
+        choice = int(input("Enter Your Choice[1-5]: "))
+        if choice == 1:
+            amount = int(input("Enter a Fix Amount to be paid every month: "))
+            set_fix_amount(amount)
+        elif choice == 2:
+            amount = int(input("Enter Transaction Amount: "))
+            transact_amount(amount)
+        elif choice == 3:
+            pass
+        elif choice == 4:
+            show_transaction_history()
+        else:
+            break
 
 
 def main():
@@ -81,3 +142,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
