67262c7dd79a3a98dae09482a468eb8aaa77772b
Ishant <er.ishant@gmail.com>
2021-09-01 11:58:56 +0530

    Session30 Introduction to DataScience

---
+++ Session30.py
@@ -0,0 +1,26 @@
+"""
+    DataScience
+        1. Numpy
+        2. Matplotlib and Seaborn
+        3. Pandas
+
+    Machine Learning
+        4. sklearn | scikit (google)
+
+    Deep Learing
+        AI
+        tensorflow, pytorch, opencv....
+
+
+   Either we work in .py i.e. python modules
+
+   >> Data Science
+   Or we work in .ipynb -> python notebook
+"""
+
+import numpy as np
+print("Version:", np.__version__)
+
+array = np.array(50)
+print(array)
+print(np.size(array))
